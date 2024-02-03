77e8808c2dbc4e93d9bc0904bce1264844f444b9
Matias <mmatidelga2@gmail.com>
2024-02-03 08:54:33 -0300

    fix: change date format and change RSS icon

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const SITE = {
+  title: "TinyRocket",
+  description:
+    "Astro theme / template. It is intended to be a starter, so it is minimally styled. You can push the design as far out as possible with Tailwind CSS.",
+  website: "https://tinyrocket.pages.dev/",
+};
